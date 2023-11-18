dc286da1fafdccf5c3fd25dbb25bbfb9eff7e632
Sushil Basi <basisushil@Sushils-MacBook-Pro.local>
2023-11-18 12:56:37 -0500

    first commit

---
+++ src/api/axios.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BASE_URL_VERSION;
+
+const parsedData = window.location.pathname.split("/");
+const domainName: any = parsedData[1];
+
+
+const axiosInstance = axios.create({
+    baseURL,
+    headers: {
+        Accept: "application/json",
+        // 'Access-Control-Allow-Origin' : '*',
+        // company_domain: domainName === "admin" ? "" : domainName,
+    },
+});
+
+axiosInstance.interceptors.response.use(
+    (response: any) => {
+        return response;
+    },
+    (err) => {
+        //Handle refresh token here
+        const originalRequest = err?.config;
+        if (err?.response?.status === 306 && !originalRequest?._retry) {
+            originalRequest._retry = true;
+            //do some stuff here........
+            return axiosInstance(originalRequest);
+        }
+        // else if(err.response.status === 400){
+        //   window.location.href = "/login";
+        // }
+
+        return Promise.reject(err);
+    }
+);
+
+export default axiosInstance;
